ce9cbb779618bd961c9b31f832f6d1b4de2d18cd
Vitaly Serov <serovvitaly@gmail.com>
2014-03-15 03:28:21 +0400

    фронтенд

--- public/app.js
+++ public/app.js
@@ -5,13 +5,17 @@ App.ApplicationView = Em.View.extend({
 });
 
 App.ApplicationAdapter = DS.RESTAdapter.extend({
-    host: '/ajax'
+    host: '/api'
 });
 
 App.Router.map(function(){
     this.resource('continents');
-    this.resource('countries');
-    this.resource('cities');
+    this.resource('countries', function(){
+        this.resource('country', { path: "/:country_id" });
+    });
+    this.resource('cities', function(){
+        this.resource('city', { path: '/:city_id' });
+    });
     this.resource('airports');
     this.resource('hotels');
     this.resource('tours');
@@ -19,15 +23,191 @@ App.Router.map(function(){
     this.resource('trails');
 });
 
-App.City = DS.Model.extend({
-    name: DS.attr('name')
+
+/**
+* Страны
+*/
+
+App.CountryModel = DS.Model.extend({
+    cid: DS.attr('number'),
+    name: DS.attr('string')
 });
 
-App.Cities = DS.Store.extend();
+App.CountryView = Ember.View.extend({
+    classNames: ['full-height'],
+    didInsertElement: function(){
+        
+        $('.intro-side').css('width', '20%');
+        $('.covert-side').css('width', '30%');
+        $('.content-side').css('width', '50%');
+        
+        $('.place-information').mCustomScrollbar({
+            theme: 'dark',
+            horizontalScroll: false,
+            autoDraggerLength: false
+        });
+        
+        return;
+        
+        $('.intro-side').animate({
+            width: '20%'
+        }, 200);
+        $('.covert-side').animate({
+            width: '30%'
+        }, 200, function(){
+            $('.place-information').mCustomScrollbar({
+                theme: 'dark',
+                horizontalScroll: false,
+                autoDraggerLength: false
+            });
+        });
+        $('.content-side').animate({
+            width: '50%'
+        }, 200);
+        
+        return;
+        
+        $('.scrollbar').mCustomScrollbar({
+            theme: 'dark-thick',
+            horizontalScroll: true,
+            autoDraggerLength: false
+        });
+    },
+    willDestroyElement: function(){
+        $('.intro-side').css('width', '25%');
+        $('.covert-side').css('width', '0%');
+        $('.content-side').css('width', '75%');
+        return;
+        $('.intro-side').animate({
+            width: '25%'
+        }, 200);
+        $('.covert-side').animate({
+            width: '0%'
+        }, 200);
+        $('.content-side').animate({
+            width: '75%'
+        }, 200);
+    }
+});
+
+App.CountriesRoute = Ember.Route.extend({
+    model: function(){
+        return this.store.find('country', {page: 1});
+    }
+});
+
+App.CountriesView = Ember.View.extend({
+    classNames: ['full-height'],
+    didInsertElement: function(){        
+        $('.scrollbar').mCustomScrollbar({
+            theme: 'dark-thick',
+            horizontalScroll: true,
+            autoDraggerLength: false
+        });
+    }
+});
+
+App.CountriesController = Ember.ArrayController.extend({
+    actions: {
+        briefAboutCity: function(){
+            //
+        }
+    }
+});
+
+
+
+
+/**
+* Города
+*/
+
+App.CityModel = DS.Model.extend({
+    cid: DS.attr('number'),
+    name: DS.attr('string')
+});
+
+App.CityView = Ember.View.extend({
+    classNames: ['full-height'],
+    didInsertElement: function(){
+        
+        $('.intro-side').css('width', '20%');
+        $('.covert-side').css('width', '30%');
+        $('.content-side').css('width', '50%');
+        
+        $('.place-information').mCustomScrollbar({
+            theme: 'dark',
+            horizontalScroll: false,
+            autoDraggerLength: false
+        });
+        
+        return;
+        
+        $('.intro-side').animate({
+            width: '20%'
+        }, 200);
+        $('.covert-side').animate({
+            width: '30%'
+        }, 200, function(){
+            $('.place-information').mCustomScrollbar({
+                theme: 'dark',
+                horizontalScroll: false,
+                autoDraggerLength: false
+            });
+        });
+        $('.content-side').animate({
+            width: '50%'
+        }, 200);
+        
+        return;
+        
+        $('.scrollbar').mCustomScrollbar({
+            theme: 'dark-thick',
+            horizontalScroll: true,
+            autoDraggerLength: false
+        });
+    },
+    willDestroyElement: function(){
+        $('.intro-side').css('width', '25%');
+        $('.covert-side').css('width', '0%');
+        $('.content-side').css('width', '75%');
+        return;
+        $('.intro-side').animate({
+            width: '25%'
+        }, 200);
+        $('.covert-side').animate({
+            width: '0%'
+        }, 200);
+        $('.content-side').animate({
+            width: '75%'
+        }, 200);
+    }
+});
+
+
+//App.CitiesModel = DS.Store.extend();
 
 App.CitiesRoute = Ember.Route.extend({
     model: function(){
-        return this.store.all(App.City);
-        return this.store.find('City');
+        return this.store.find('city', {page: 1});
+    }
+});
+
+App.CitiesView = Ember.View.extend({
+    classNames: ['full-height'],
+    didInsertElement: function(){        
+        $('.scrollbar').mCustomScrollbar({
+            theme: 'dark-thick',
+            horizontalScroll: true,
+            autoDraggerLength: false
+        });
+    }
+});
+
+App.CitiesController = Ember.ArrayController.extend({
+    actions: {
+        briefAboutCity: function(){
+            //
+        }
     }
 });
