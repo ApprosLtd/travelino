40031e22c703493d287ae45521d60b3484106516
Vitaly Serov <serovvitaly@gmail.com>
2014-03-16 00:55:56 +0400

    конфиг пакета oauth

---
+++ app/config/oauth.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+    '500px' => array(
+    
+        'consumer_key'    => 'nTGaUyu6ZASP5DtWYXiPmz4L3sx1BmgcpkMzW5s4',
+        
+        'consumer_secret' => 'b3GAyqw6p9asHdXjDuG6g9AUgTR9Yk0cnhbpCu73',
+        
+    )
+
+);
